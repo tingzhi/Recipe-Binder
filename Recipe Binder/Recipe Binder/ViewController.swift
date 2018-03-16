e7d77f339fe9be93abb8202cd7fd701d67d10497
Tingzhi Li <tingzhi.li6@gmail.com>
2018-03-16 03:09:51 -0700

    Initial commit. Ground breaking.

---
+++ Recipe Binder/Recipe Binder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipe Binder
+//
+//  Created by Tingzhi Li on 3/16/18.
+//  Copyright © 2018 tingzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
